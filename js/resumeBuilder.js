09514b72cae572c441487cb943bf07e70465d439
illuminatedspace <anfractua@gmail.com>
2016-11-10 16:38:26 -0500

    adds work through Data Types-3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append("Liz Phillips");
+
+var firstName = "Liz";
+var age = 25;
+
+console.log(firstName);
+
+var awesomeThoughts = "I am " + firstName + " and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+var email = "lizkristinaphillips@gmail.com";
+var newEmail = email.replace ("gmail", "hired");
+
+console.log(email);
+console.log(newEmail);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+console.log(funThoughts);
+
+$("#main").append(funThoughts);
